25109559d01a003ff78750dabdde57b4ef340488
JoeLee <joe@cybavo.com>
2020-10-29 15:48:47 +0800

    Add minimum node.js version check

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var semver = require('semver')
 
 var indexRouter = require('./routes/index');
 var walletsRouter = require('./routes/wallets');
@@ -16,5 +17,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/v1/mock/wallets', walletsRouter);
+
+if (!semver.satisfies(process.version, '>=v15.19.0')) {
+  console.log(`
+  ***********************************************************************
+  **                                                                   **
+  **  Required version: node v10.19.0 or later (npm v6.13.4 or later)  **
+  **                                                                   **
+  ***********************************************************************
+
+  program exited!`);
+  process.exit();
+}
 
 module.exports = app;
