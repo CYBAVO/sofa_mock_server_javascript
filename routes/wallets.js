ff26da9f96dbd542a7d1a4fe783ff4d7f33c981c
JoeLee <joe@cybavo.com>
2021-05-29 00:26:25 +0800

    Add notification endpoint inspection URL

--- routes/wallets.js
+++ routes/wallets.js
@@ -667,4 +667,19 @@ router.get('/readonly/walletlist', async function(req, res) {
   }
 });
 
+router.post('/:wallet_id/notifications/inspect', async function(req, res) {
+  if (!req.params.wallet_id) {
+    res.status(400).json({ error: 'invalid parameters' });
+    return;
+  }
+  const apires = await api.makeRequest(req.params.wallet_id, "POST",
+    `/v1/sofa/wallets/${req.params.wallet_id}/notifications/inspect`,
+    null, JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
 module.exports = router;
